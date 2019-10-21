abf21aec66d1d28132b9b86614407e1e5efe3fa2
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-10-21 17:45:02 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// console.log("<h1>Welcome P213</h1>");
+// document.write("<h1>Welcome P213</h1>");
+// var x = '5';
+// var y = 4;
+
+// var y = 7;
+
+// var a = 5;
+// let name = a;
+// a = 6;
+
+// const pi = 3.14;
+// const pi = 4;
+
+// let a = 6;
+// let b = a;
+
+// console.log(b);
+
+// var age = 18;
+
+// var result = (age > 20) ? "Axhsamlar olar" : "Olmaz!!!";
+// console.log(result)
+
+// if (age > 20) {
+//     console.log("Axhsamlar olar")
+// } else if (age == 19) {
+//     console.log("Belke")
+// } else {
+//     console.log("Olmaz!!!")
+// }
+
+// var age = 24;
+// var name = "Memmed";
+// var city = "Sheki";
+
+// if (age > 25) {
+//     if (city == "Baki") {
+//         console.log("Qebir pullu olacaq")
+//     } else {
+//         console.log("Belke havayi oldu")
+//     }
+// }
+
+// var login = "user";
+// var parol = 1234;
+
+// if (!(login == "user" || parol == 123)) {
+//     console.log("Xosh geldiniz")
+// } else {
+//     console.log("Bextinizi bir daha sinayin")
+// }
+
+let a = 5000;
+let b = 100;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+
+console.log("a-nin deyeri: " + a);
+console.log("b-nin deyeri: " + b);
